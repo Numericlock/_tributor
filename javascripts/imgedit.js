6c648363a7528dee3070f14d7bb9859a163b613c
yamamotonyan <57880393+yamamotonyan@users.noreply.github.com>
2019-11-25 23:56:39 +0900

    Shingo Edit

---
+++ javascripts/imgedit.js
@@ -0,0 +1,11 @@
+$("#hoge").click(function(){
+   console.log("aaaa");
+});
+
+$('#myImage').on('change', function (e) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $("#preview").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+});
