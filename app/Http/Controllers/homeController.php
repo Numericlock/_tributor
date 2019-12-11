94cbf7483b99d789f6611e58f47da03a3eee91c7
yamamotonyan <57880393+yamamotonyan@users.noreply.github.com>
2019-12-11 23:08:58 +0900

    Update homeController.php

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -23,9 +23,9 @@ public function __construct()
     
 	
     public function home (Request $request){
-		$posts = User_post::select('users_posts.*','users.id','users.name')
+		$posts = User_post::select('users_posts.*','users.*')
 		->join('users_follows', 'users_follows.followed_user_id', '=', 'users_posts.post_user_id')
-        ->join('users_posts', 'users_posts.post_user_id', '=', 'users.id')
+        ->join('users', 'users.id', '=', 'users_posts.post_user_id')    
 		->join('posts_vaild_disclosure_lists', 'users_posts.id', '=', 'posts_vaild_disclosure_lists.post_id')
 		->join('disclosure_lists_users', 'disclosure_lists_users.list_id', '=', 'posts_vaild_disclosure_lists.list_id')
 		->where('users_follows.subject_user_id',$request->base_user->user_id)
@@ -34,7 +34,7 @@ public function __construct()
 		Log::debug($posts."foloowwwdｗｗｗｗｗｗだああｗｗｗ2");
         $user = $request->base_user;
 		$lists = $request->base_user_lists;
-		return view('home',compact('user','lists'));
+		
 		return view('home',compact('posts','user','lists'));
 
 	}
